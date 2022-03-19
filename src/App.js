8e27004ffed812a82e0ae4df5961e4d4521a2f96
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2022-03-19 09:06:40 -0400

    refactor in separate components

--- src/App.js
+++ src/App.js
@@ -19,30 +19,43 @@ const list = [
     },
 ];
 
-
 function App() {
     return (
         <div>
             <h1>My Hacker Stories</h1>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
-
+            
+            <Search />
+            
             <hr />
             
-            {/* render list here */}
-            <ul>
-                {list.map(item =>
-                    <li key={item.objectID}>
-                        <span>
-                            <a href={item.url}>{item.title}</a>
-                        </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </li>
-                )}
-            </ul>
+            <List />
         </div>
+    );
+}
+
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
+
+function List() {
+    return (
+        <ul>
+            {list.map(item =>
+                <li key={item.objectID}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                </li>
+            )}
+        </ul>
     );
 }
 
