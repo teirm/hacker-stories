300cd152aaca9e9a702bc85eea16da04d6a09696
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2022-03-27 09:09:06 -0400

    reversible list

--- src/App.tsx
+++ src/App.tsx
@@ -292,14 +292,18 @@ type ListProps = {
 const List = React.memo(
     ({list, onRemoveItem}: ListProps) => {
         
-    const [sort, setSort] = React.useState('NONE');
+    const [sort, setSort] = React.useState({
+        sortKey: 'NONE',
+        isReverse: false,
+    });
 
     const handleSort = (sortKey: string) => {
-        setSort(sortKey);
+        const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+        setSort({sortKey, isReverse});
     }; 
     
-    const sortFunction = SORTS[sort];
-    const sortedList = sortFunction(list);
+    const sortFunction = SORTS[sort.sortKey];
+    const sortedList = sort.isReverse ? sortFunction(list).reverse() : sortFunction(list);
 
     return (
         <ul>
